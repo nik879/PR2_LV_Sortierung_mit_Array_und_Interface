ac83b79a7e103ba5f750ed0129ef9e3350a0491f
DESKTOP-FISKTM0\nikik <niki.kachelmaier@gmail.com>
2021-03-29 15:37:10 +0200

    ausgabe

--- src/nik879/SortInterface/Arrayhelper.java
+++ src/nik879/SortInterface/Arrayhelper.java
@@ -10,19 +10,31 @@ public class Arrayhelper {
 
     public static void arrangeOrder(int[] numbers) {
 
-        System.out.println("numbers unsortiert = " + numbers.toString());
+        System.out.println("Unsortiert:");
+
+        for (int number : numbers) {
+            System.out.print(number +" ");
+        }
+
+        System.out.println("\n"+"---");
 
         int temp=0;
 
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers.length-1; j++) {
-                if (numbers[i] > numbers[i + 1]) {
-                    temp = numbers[i];
-                    numbers[i] = numbers[i + 1];
-                    numbers[i + 1] = temp;
+                if (numbers[j] > numbers[j + 1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
                 }
             }
         }
-        System.out.println("numbers sortiert = " + numbers.toString());
+        System.out.println("sortiert:");
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
     }
+
+
 }
